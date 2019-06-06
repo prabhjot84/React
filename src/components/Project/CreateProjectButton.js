8a3b24528bb43203e8c87cbaa9040e2b5a9016b8
psingh84 <prabhjot.singh84@t-mobile.com>
2019-06-06 13:38:31 -0700

    initial

---
+++ src/components/Project/CreateProjectButton.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class CreateProjectButton extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Link to="/addProject" className="btn btn-lg btn-info">
+          Create a Project
+        </Link>
+      </React.Fragment>
+    );
+  }
+}
+export default CreateProjectButton;
