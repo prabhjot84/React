06c04b778b3ac7f43f7f4b082c69714c347419bb
psingh84 <prabhjot.singh84@t-mobile.com>
2019-06-10 14:21:12 -0700

    test

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import errorReducer from "./errorReducers";
-import projectReducer from "./projectReducer";
-
-export default combineReducers({
-  errors: errorReducer,
-  project: projectReducer
-});
